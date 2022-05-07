0285cc39036598131c84f4642d6774a2da6226ab
kisekifrans <agisswag23@gmail.com>
2022-05-07 22:18:54 +0800

    Anjay..

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  Widget build(context) {
+    return MaterialApp(
+      title: 'Login mazseh',
+      home: Scaffold(
+        body: Text('Hmmmmmm..'),
+      ),
+    );
+  }
+}
